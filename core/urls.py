23174a5e472446449b9cccb518e56ac3b4225879
Vedprakash Upraity <vedprakashupraity@gmail.com>
2020-02-16 23:24:26 +0530

    Remove log statements from core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,17 +5,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-
-import logging
-
-
-logger = logging.getLogger(__name__)
-
-
-def sample_logging():
-    logger.debug('logger.debug')
-    logger.error('logger.error')
-
-
-sample_logging()
